54c33cf89d2d03ae1ae1f8b608a229141fd9e382
7264985088 <70712704+7264985088@users.noreply.github.com>
2020-09-08 20:44:16 +0530

    Assignment3 day5(Batch7) Jupyter notebook

---
+++ Assignment3(day5).py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # uppercase the sentence
+# 
+#            
+#         
+
+# In[9]:
+
+
+lst = ["I am learning python","hello","good evening"]
+lst1= map(lambda x:x.title(),lst)
+list(lst1)
+        
+
+
+# In[ ]:
+
+
+
+
