97abac664cd3130ed22b0c08abdc129457194c13
7264985088 <70712704+7264985088@users.noreply.github.com>
2020-09-09 08:42:13 +0530

    Assignment2 Day6 (Batch7) jupyter nootebook

---
+++ Assignment2 Day6.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Cone Class
+
+# In[12]:
+
+
+from math import sqrt,pi
+class cone:
+    def __init__(self,radius,height,):
+        self.radius = radius
+        self.height = height
+    def methodA(self):
+        print("volume: ",pi*(c.radius**2)*(c.height/3))
+        print("surface area of base: ",3.14*(self.radius**2))
+        print("surface area of side: ",3.14*c.radius*sqrt((self.radius**2)+(self.height**2)))
+        
+   
+        
+   
+
+
+# In[14]:
+
+
+
+c = cone(40,12)
+print("radius is: ",c.radius)
+print("height is: ",c.height)
+c.methodA()     
+     
+     
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
